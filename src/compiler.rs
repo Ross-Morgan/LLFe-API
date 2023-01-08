62c57d691400bb055a2118109a8b416f2d53ea12
Ross Morgan <morgan.ross.512@gmail.com>
2023-01-08 13:30:50 +0000

    commit

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,5 +1,7 @@
 use errors::LLFeError;
 
-pub fn compile_from_source() -> Result<(), LLFeError> {
+pub fn compile_from_source(source: String) -> Result<(), LLFeError> {
+    dbg!(source);
+
     Ok(())
 }
